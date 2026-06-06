42622c623ed9fe2d1c5ff1433dd39da9faabe747
Sujeet12000 <sujeetc1029@gmail.com>
2026-06-06 23:33:27 +0530

    Connect frontend to Render backend

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,7 +1,8 @@
 // frontend/src/socket/socket.js
 import { io } from 'socket.io-client';
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:5000';
+const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL ?? "https://codesync.onrender.com";
 
 const socket = io(BACKEND_URL, {
   // Don't connect until we explicitly call socket.connect()
